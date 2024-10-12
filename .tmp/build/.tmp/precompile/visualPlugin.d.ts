9587be6dbf96642afff351fab1dd57a09cdaf8ab
kyber1 <leonard.merva@gmail.com>
2024-10-12 14:24:51 +0200

    deleting it so its not in the branch online

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var candlestickchartCBC86C3F2C914CD3BDF86EFE7F985E9A_DEBUG: IVisualPlugin;
-export default candlestickchartCBC86C3F2C914CD3BDF86EFE7F985E9A_DEBUG;
